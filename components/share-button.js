ca09383f1c0528a4a3817a58a8c7656518218547
greg-47 <greg.47@kakaocorp.com>
2026-05-31 22:30:08 +0900

    Refine share icon style

--- components/share-button.js
+++ components/share-button.js
@@ -34,13 +34,13 @@ export default function ShareButton({
         height="18"
         fill="none"
         stroke="currentColor"
-        strokeWidth="1.8"
+        strokeWidth="1.7"
         strokeLinecap="round"
         strokeLinejoin="round"
       >
-        <path d="M8 12l8-8" />
-        <path d="M10 4h6v6" />
-        <path d="M20 14v4a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h4" />
+        <path d="M14 4h6v6" />
+        <path d="M10 14 20 4" />
+        <path d="M20 13v5a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5" />
       </svg>
       {iconOnly ? <span className="sr-only">{label}</span> : <span>{label}</span>}
     </button>
